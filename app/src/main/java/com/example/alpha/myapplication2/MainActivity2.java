9986ffc46609d16d1b1d4286a93e545980ef2256
alpha <kiranmanealphaict@gmail.com>
2017-06-07 15:32:32 +0530

    testing

--- app/src/main/java/com/example/alpha/myapplication2/MainActivity2.java
+++ app/src/main/java/com/example/alpha/myapplication2/MainActivity2.java
@@ -1,9 +1,9 @@
 package com.example.alpha.myapplication2;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity2 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
